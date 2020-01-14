2250f3c2d2575cdf6a5771df238951ef61cb0bca
Rehman Ali <rehmanali001@gmail.com>
2020-01-14 23:05:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,20 +1,20 @@
 class SongsController < ApplicationController
     before_action :set_song!, only: [:show, :edit, :update, :destroy]
- 
+
      def index
        @songs = Song.all
      end
- 
+
      def show
      end
- 
+
      def new
        @song = Song.new
      end
- 
+
      def edit
      end
- 
+
      def create
        @song = Song.new(song_params)
        if @song.save
@@ -23,7 +23,7 @@ class SongsController < ApplicationController
          render :new
        end
      end
- 
+
      def update
        if @song.update(song_params)
          redirect_to @song
@@ -31,20 +31,20 @@ class SongsController < ApplicationController
          render :edit
        end
      end
- 
+
      def destroy
        @song.destroy
        redirect_to songs_url
      end
- 
+
      private
- 
+
      def song_params
        params.require(:song).permit(:title, :release_year, :released, :genre, :artist_name)
      end
- 
+
      def set_song!
          @song = Song.find(params[:id])
      end
- 
+
    end 
